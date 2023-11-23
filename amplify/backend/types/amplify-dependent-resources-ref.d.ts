0190ca88979a4feda26a544f7eb1ba724ed1a28b
Austin O <guitarguy199@gmail.com>
2023-11-23 12:59:01 -0700

    add graphql and ui

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifyreact": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "awsamplifyreact5b06aa32": {
       "AppClientID": "string",
